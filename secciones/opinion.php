850ebce02a1a655e10dc1ea27dad3904b7cc92a0
diegoAvaloz <diego.avalos.chong@gmail.com>
2025-10-28 17:29:07 -0500

    Add files via upload

---
+++ secciones/opinion.php
@@ -0,0 +1,12 @@
+<?php
+/*****
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+*****/
+
+$opinion = [
+"titulo" => "¿Dónde nos informamos los peruanos? Los medios más consumidos este 2024",
+"autor"  => "Juan Ramirez",
+"resumen"=> "Un reciente informe de Kantar IBOPE Media que detalla el consumo de medios entre los peruanos durante el 2024 ha revelado interesantes tendencias en las preferencias de la población en cuanto a lo que ven, leen y escuchan.",
+"imagen" => "images/opinion_title.png",
+];
+?>
---
+++ secciones/portada.php
@@ -0,0 +1,11 @@
+<?php
+/*****
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+*****/
+
+$portada = [
+"titulo" => "Al último momento",
+"autor" => "jmirko",
+"resumen" => "Las noticias mas importantes del 2025",
+];
+?>
